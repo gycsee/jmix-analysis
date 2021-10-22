ea45a40d0809a0cb0fb129f0a683b70c5032ccea
Yachen Gan <yachen@shanshu.ai>
2021-10-22 11:38:39 +0800

    feat: Add redux store in global decorators

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,22 @@
+import { Provider } from 'react-redux';
+
+import { store } from '../src/app/store';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
+    expanded: true,
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
 }
+
+export const decorators = [
+  (Story) => (
+    <Provider store={store}>
+      <Story />
+    </Provider>
+  ),
+];
